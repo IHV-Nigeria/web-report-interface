cc151fc58aabfa2ea53ea2f2b80ad8fd6d7c95ee
tayoraimi <tayo.raimi@gmail.com>
2022-05-13 12:12:42 +0100

    Update index.js
    Added Highchart

--- src/views/pbs-tracker/list/index.js
+++ src/views/pbs-tracker/list/index.js
@@ -1,5 +1,5 @@
 // ** React Imports
-import { Fragment, useState, useContext } from 'react'
+import { Fragment, useState} from 'react'
 
 import RevenueReport from '@src/views/ui-elements/cards/analytics/RevenueReport'
 
@@ -22,9 +22,47 @@ import LineListTable from './lineListTable'
 // ** Styles
 import '@styles/react/apps/app-users.scss'
 
+const options = {
+  chart: {
+    stacked: true,
+    type: 'column',
+    toolbar: { show: false }
+  },
+  title: {
+    text: 'PBS Achievement'
+  },
+  xAxis: {
+    categories: ['FCT', 'Katsina', 'Nasarawa', 'Rivers']
+},
+plotOptions: {
+  
+  column: {
+    borderWidth: 0,
+    pointWidth: 70
+}
+},
+series: [
+  {
+    type: 'column',
+    name: 'TxCurr',
+    data: [300, 280, 1000, 300]
+}, {
+    type: 'column',
+    name: 'Captured',
+    data: [148, 100, 500, 149]
+}, {
+    type: 'column',
+    name: 'Usable',
+    data: [148, 100, 500, 149]
+}
+
+]
+}
+
+
 const UsersList = () => {
   const [sidebarOpen] = useState(false)
-  const context = useContext(ThemeColors)
+  //const context = useContext(ThemeColors)
   return (
     <div className='app-user-list'>
       <IndicatorFilter sidebarOpen={sidebarOpen} />
@@ -62,12 +100,13 @@ const UsersList = () => {
             renderStats={<h3 className='fw-bolder mb-75'>5,678</h3>}
           />
         </Col>
-        
-        
       </Row>
+      
       <Row className='match-height'>
       <Col lg='12' sm='12'>
-            <RevenueReport primary={context.colors.primary.main} />
+      <div>
+    <HighchartsReact highcharts={Highcharts} options={options} />
+  </div>
           </Col>
           
       </Row>
