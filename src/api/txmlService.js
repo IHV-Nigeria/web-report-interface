43f0d2456baa9c486e8d64dfad8f21369affe51c
Temiye Oluseun <6815443+wonderfulguru1@users.noreply.github.com>
2022-10-19 11:03:09 +0100

    updates
    updates

--- src/api/txmlService.js
+++ src/api/txmlService.js
@@ -1,17 +1,11 @@
 import apiRequest from './useJwt'
+import {serialize} from '../api/utils/common'
+
 import {buildWaterFallChart} from './chatUtils/waterfallChat'
 
 
 export const fetchGetTXMLAnalytics = (param) => {
-    const url = `data/get-txml-analytics?states=${param.states}&
-    lgas=${param.lgas}&
-    facilities=${param.facilities}&
-    ageRange=${param.ageRange}&
-    indicator=${param.indicator}&
-    sex=${param.sex}&
-    searchType=NORMAL&
-    startDate=${param.startDate}&
-    endDate=${param.endDate}`
+    const url = `data/get-txml-analytics?${serialize(param)}`
     return apiRequest({
        requetType: 'GET',
        contentType: 'application/json',
